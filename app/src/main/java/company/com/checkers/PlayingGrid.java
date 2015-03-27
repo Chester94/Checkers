8f14fa7e92979734ebe757b551f64cc52e893b33
Frolov_Daniil <FrolovD94@gmail.com>
2015-03-27 20:36:22 +0400

    Корректная работа класса отрисовки

--- app/src/main/java/company/com/checkers/PlayingGrid.java
+++ app/src/main/java/company/com/checkers/PlayingGrid.java
@@ -4,15 +4,16 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class PlayingGrid extends View {
-    public PlayingGrid(Context context) {
-        super(context);
+    public PlayingGrid(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    public void onDraw(Canvas canvas) {
         Paint paint = new Paint();
         paint.setColor(Color.RED);
         canvas.drawLine(10, 10, 100, 100,paint);
