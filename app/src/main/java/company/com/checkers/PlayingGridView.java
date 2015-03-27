2fe2849ae71cb2db8b7040d8bd2cdeaf80c83f35
Frolov_Daniil <FrolovD94@gmail.com>
2015-03-27 21:13:18 +0400

    Вывод круга на экран

--- app/src/main/java/company/com/checkers/PlayingGridView.java
+++ app/src/main/java/company/com/checkers/PlayingGridView.java
@@ -5,18 +5,45 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
-public class PlayingGridView extends View {
+public class PlayingGridView extends View implements View.OnTouchListener {
+    private double x;
+    private double y;
+
     public PlayingGridView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setOnTouchListener(this);
+
+        x = -100;
+        y = -100;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
         paint.setColor(Color.RED);
+        canvas.drawCircle((float)x, (float)y, 40, paint);
+    }
 
-        canvas.drawLine(10, 10, 200, 200, paint);
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x = event.getX();
+                y = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                x = event.getX();
+                y = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x = -100;
+                y = -100;
+                break;
+        }
+        invalidate();
+        return true;
     }
 }
