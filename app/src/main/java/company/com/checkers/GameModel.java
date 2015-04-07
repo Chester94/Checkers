ea0d762c22666ca638cdafd8733478fdfe2cc8a8
Frolov_Daniil <FrolovD94@gmail.com>
2015-04-07 22:07:49 +0400

    Добавлена документация для модели

--- app/src/main/java/company/com/checkers/GameModel.java
+++ app/src/main/java/company/com/checkers/GameModel.java
@@ -1,41 +1,116 @@
 package company.com.checkers;
 
+/**
+ * Interface to handle the situation of losing.
+ */
 interface Lose {
     void losing();
 }
 
+/**
+ * The main application class.
+ * Stores information about the current state of the game.
+ * Playing field, the current progress, active (raised) chip handles moves,
+ * attacks and multiple attacks.
+ */
 public class GameModel {
+    /**
+     * The dimensions of the playing field.
+     */
     public static final int GRID_DIMENSION = 8;
 
+    /**
+     * checkers no.
+     */
     public static final int NONE = 0;
+
+    /**
+     * White checker.
+     */
     public static final int WHITE = 1;
+
+    /**
+     * Black checker.
+     */
     public static final int BLACK = 2;
+
+    /**
+     * White queen checker
+     */
     public static final int WHITE_QUEEN = 3;
+
+    /**
+     * Black queen checker
+     */
     public static final int BLACK_QUEEN = 4;
 
+    /**
+     * Current turn color.
+     */
     private int turn = WHITE;
 
+    /**
+     * Game field.
+     */
     private int[][] grid;
 
+    /**
+     * Row active checkers
+     */
     private int activeCheckerRow = -1;
+
+    /**
+     * Column active checkers
+     */
     private int activeCheckerColumn = -1;
+
+    /**
+     * true - there is an active checker
+     * false - no active checkers
+     */
     private boolean active = false;
 
+
+    /**
+     * Row checkers who have committed multiple attack
+     */
     private int comboAttackerRow = -1;
+
+    /**
+     * Column checkers who have committed multiple attack
+     */
     private int comboAttackerColumn = -1;
+
+    /**
+     * true - Occurs Multiattack
+     * false - There is not a multiple attack
+     */
     private boolean attackStreak = false;
 
+    /**
+     * Handler loss
+     */
     private Lose loserListener = null;
 
+    /**
+     * Constructor.
+     */
     public GameModel() {
         grid = new int [GRID_DIMENSION][GRID_DIMENSION];
         initGame();
     }
 
+    /**
+     * Setter
+     * @param loserListener handler loss
+     */
     public void setLoserListener(Lose loserListener) {
         this.loserListener = loserListener;
     }
 
+    /**
+     * Initialization of the game. Assigning initial values.
+     */
     public void initGame() {
         turn = WHITE;
 
@@ -64,6 +139,11 @@ public class GameModel {
         }
     }
 
+    /**
+     * Start step.
+     * @param row row checkers
+     * @param column column checkers
+     */
     public void startStep(int row, int column) {
         if (isOutOfField(row, column))
             return;
@@ -83,6 +163,11 @@ public class GameModel {
         }
     }
 
+    /**
+     * Stop step.
+     * @param row row checkers
+     * @param column column checkers
+     */
     public void stopStep(int row, int column) {
         if (!isActiveDragging())
             return;
@@ -100,14 +185,26 @@ public class GameModel {
         deactivateDragging();
     }
 
+    /**
+     * Getter.
+     * @return current turn color
+     */
     public int getTurn() {
         return turn;
     }
 
+    /**
+     * Getter.
+     * @return row activated checkers
+     */
     public int getActiveCheckerRow() {
         return activeCheckerRow;
     }
 
+    /**
+     * Getter.
+     * @return column activated checkers
+     */
     public int getActiveCheckerColumn() {
         return activeCheckerColumn;
     }
@@ -164,12 +261,20 @@ public class GameModel {
         this.attackStreak = attackStreak;
     }
 
+    /**
+     * "Raises" checker
+     * @param row row activated checkers
+     * @param column column activated checkers
+     */
     private void activateDragging(int row, int column) {
         activeCheckerRow = row;
         activeCheckerColumn = column;
         active = true;
     }
 
+    /**
+     * "lowers" checker
+     */
     private void deactivateDragging() {
         activeCheckerRow = -1;
         activeCheckerColumn = -1;
@@ -184,6 +289,10 @@ public class GameModel {
         return activeCheckerRow == row && activeCheckerColumn == column;
     }
 
+    /**
+     * Verifying current player to make a move checkers
+     * @return available - true, non-available - false
+     */
     private boolean areMovesAvailableForPlayer() {
         for (int i = 0; i < GRID_DIMENSION; i++) {
             for (int j = 0; j < GRID_DIMENSION; j++) {
@@ -194,6 +303,12 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * Verifying current player to make a certain move checkers
+     * @param row row checkers
+     * @param column column checkers
+     * @return available - true, non-available - false
+     */
     private boolean areMovesAvailable(int row, int column) {
         boolean movesAvailable = false;
 
@@ -212,6 +327,12 @@ public class GameModel {
         return movesAvailable;
     }
 
+    /**
+     * Verifying current player to make a certain move checkers up
+     * @param row row checkers
+     * @param column column checkers
+     * @return available - true, non-available - false
+     */
     private boolean areMovesAvailableAbove(int row, int column) {
         if (row == 0)
             return false;
@@ -225,6 +346,12 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * Verifying current player to make a certain move checkers down
+     * @param row row checkers
+     * @param column column checkers
+     * @return available - true, non-available - false
+     */
     private boolean areMovesAvailableBelow(int row, int column) {
         if (row == (GRID_DIMENSION - 1))
             return false;
@@ -238,6 +365,10 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * Verifying current player to make any attack with his checker
+     * @return available - true, non-available - false
+     */
     private boolean areAttackAvailableForPlayer() {
         for (int i = 0; i < GRID_DIMENSION; i++) {
             for (int j = 0; j < GRID_DIMENSION; j++) {
@@ -249,6 +380,12 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * Verifying current player to make certain checker attack
+     * @param row row checkers
+     * @param column column checkers
+     * @return available - true, non-available - false
+     */
     private boolean areAttackAvailable(int row, int column) {
         if (checkerOwner(row, column) != turn)
             return false;
@@ -271,6 +408,12 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * Check the availability of active checkers move to a point of the field
+     * @param toRow  to row checkers
+     * @param toColumn to column checkers
+     * @return valid - true, non-valid - false
+     */
     private boolean isMoveValid(int toRow, int toColumn) {
         if (isOutOfField(toRow, toColumn))
             return false;
@@ -292,6 +435,14 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * Check the availability of a specific attack checkers point field
+     * @param fromRow from row checkers
+     * @param fromColumn from column checkers
+     * @param toRow to row checkers
+     * @param toColumn to column checkers
+     * @return valid - true, non-valid - false
+     */
     private boolean isAttackValid(int fromRow, int fromColumn, int toRow, int toColumn) {
         if (isOutOfField(toRow, toColumn))
             return false;
@@ -311,6 +462,12 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * coordinates of the field should not go beyond its borders
+     * @param row row checkers
+     * @param column to column checkers
+     * @return out - true
+     */
     private boolean isOutOfField(int row, int column) {
         if( row < 0 || row > (GRID_DIMENSION - 1) ||
                 column < 0 || column > (GRID_DIMENSION - 1))
@@ -319,6 +476,12 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * The owner of a specific checkers
+     * @param row row checkers
+     * @param column column checkers
+     * @return player (turn) color
+     */
     private int checkerOwner(int row, int column) {
         if (grid[row][column] == WHITE || grid[row][column] == WHITE_QUEEN)
             return WHITE;
@@ -329,6 +492,11 @@ public class GameModel {
         return NONE;
     }
 
+    /**
+     * The conversion of the queen, if possible
+     * @param row row checkers
+     * @param column column checkers
+     */
     private void promoteToQueen(int row, int column) {
         if (turn == WHITE && row == 0)
             grid[row][column] = WHITE_QUEEN;
@@ -337,6 +505,9 @@ public class GameModel {
             grid[row][column] = BLACK_QUEEN;
     }
 
+    /**
+     * Change turn and check for loss
+     */
     private void changeTurn() {
         if (turn == WHITE) {
             turn = BLACK;
@@ -353,6 +524,11 @@ public class GameModel {
         attackStreak = false;
     }
 
+    /**
+     * Attack specific cells activated checker
+     * @param toRow to row checkers
+     * @param toColumn to column checkers
+     */
     private void attack(int toRow, int toColumn) {
         grid[toRow][toColumn] = grid[activeCheckerRow][activeCheckerColumn];
         grid[(toRow + activeCheckerRow) / 2][(toColumn + activeCheckerColumn) / 2] = NONE;
@@ -372,6 +548,11 @@ public class GameModel {
         }
     }
 
+    /**
+     * Motion Activated checkers into specific cell
+     * @param toRow to row checkers
+     * @param toColumn to column checkers
+     */
     private void move(int toRow, int toColumn) {
         grid[toRow][toColumn] = grid[activeCheckerRow][activeCheckerColumn];
         grid[activeCheckerRow][activeCheckerColumn] = NONE;
@@ -381,12 +562,21 @@ public class GameModel {
         changeTurn();
     }
 
+    /**
+     * Sobit loss
+     */
     private void throwLose() {
         if (loserListener != null) {
             loserListener.losing();
         }
     }
 
+    /**
+     * Belongs to the player's checkers
+     * @param row row checkers
+     * @param column column checkers
+     * @return belong - true, else - false
+     */
     private boolean isCheckerBelongsToPlayer(int row, int column) {
         if (turn == WHITE)
             return grid[row][column] == WHITE || grid[row][column] == WHITE_QUEEN;
